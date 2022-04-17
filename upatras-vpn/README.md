c7c9634d45b470a0b567e13a0eee672012aa4618
Thanos Panagiotidis <thanospan@users.noreply.github.com>
2022-04-17 10:03:15 +0300

    Add upatras-vpn

---
+++ upatras-vpn/README.md
@@ -0,0 +1,8 @@
+# upatras-vpn
+
+## Usage
+```
+sudo wget -P /usr/local/bin/ https://raw.githubusercontent.com/thanospan/scripts/main/upatras-vpn/upatras-vpn.sh
+sudo chmod +x /usr/local/bin/upatras-vpn.sh
+sudo upatras-vpn.sh
+```
---
+++ upatras-vpn/upatras-vpn.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+set -e
+
+show_help () {
+  echo "Usage: sudo upatras-vpn.sh"
+}
+
+check_permissions () {
+  if [[ $EUID -ne 0 ]]; then
+     echo "---Run with root privileges"
+     show_help
+     exit
+  fi
+}
+
+get_username () {
+  read -p "Username: " USERNAME
+}
+
+get_password () {
+  read -s -p "Password: " PASSWORD
+  echo
+}
+
+save_cookies () {
+  echo "---Saving cookies.txt..."
+  curl -s -o /dev/null -d "username=$USERNAME" -d "password=$PASSWORD" -c cookies.txt "https://mussa.upnet.gr/user/"
+  echo "---cookies.txt saved!"
+}
+
+download_file () {
+  echo "---Downloading UPatras.ovpn..."
+  curl -s -b cookies.txt "https://mussa.upnet.gr/user/index.php?action=downloadFile&fn=ovpncnf" -o /etc/openvpn/UPatras.ovpn
+  echo "---UPatras.ovpn downloaded!"
+}
+
+delete_cookies () {
+  echo "---Deleting cookies.txt..."
+  rm cookies.txt
+  echo "---cookies.txt deleted!"
+}
+
+connect () {
+  echo "---Connecting to UPatras VPN..."
+  openvpn --config /etc/openvpn/UPatras.ovpn --auth-user-pass <(echo -e $USERNAME"\n"$PASSWORD)
+}
+
+main () {
+  check_permissions
+  get_username
+  get_password
+  if [[ ! -f /etc/openvpn/UPatras.ovpn ]]; then
+    save_cookies
+    download_file
+    delete_cookies
+  fi
+  connect
+}
+
+main "$@"
