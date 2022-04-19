ad33a836f8af885931bdb438106b70e33393a1a0
Thanos Panagiotidis <thanospan@users.noreply.github.com>
2022-04-19 12:59:21 +0300

    Add setup script

---
+++ README.md
@@ -0,0 +1,9 @@
+# Scripts
+
+## Usage
+```
+git clone https://github.com/thanospan/scripts.git
+cd scripts
+chmod +x setup.sh
+setup.sh
+```
---
+++ setup.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set -e
+
+show_help () {
+  echo "---Usage: sudo $0"
+}
+
+check_permissions () {
+  if [[ $EUID -ne 0 ]]; then
+     echo "---Run with root privileges"
+     show_help
+     exit
+  fi
+}
+
+setup () {
+  echo "---Setup starting..."
+  echo
+
+  # docker-cleanup
+  sudo wget -P /usr/local/bin/ https://raw.githubusercontent.com/thanospan/scripts/main/docker-cleanup/docker-cleanup.sh
+  sudo chmod +x /usr/local/bin/docker-cleanup.sh
+
+  # laptop-lid
+  sudo wget -P /usr/local/bin/ https://raw.githubusercontent.com/thanospan/scripts/main/laptop-lid/laptop-lid.sh
+  sudo chmod +x /usr/local/bin/laptop-lid.sh
+
+  # night-light
+  sudo wget -P /usr/local/bin/ https://raw.githubusercontent.com/thanospan/scripts/main/night-light/night-light.sh
+  sudo chmod +x /usr/local/bin/night-light.sh
+
+  # upatras-vpn
+  sudo wget -P /usr/local/bin/ https://raw.githubusercontent.com/thanospan/scripts/main/upatras-vpn/upatras-vpn.sh
+  sudo chmod +x /usr/local/bin/upatras-vpn.sh
+
+  # vga2hdmi
+  sudo wget -P /usr/local/bin/ https://raw.githubusercontent.com/thanospan/scripts/main/vga2hdmi/vga2hdmi.sh
+  sudo chmod +x /usr/local/bin/vga2hdmi.sh
+
+  echo "---Done!"
+}
+
+main () {
+  check_permissions
+  setup
+}
+
+main
